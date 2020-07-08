fe732a1809332537c5554a35101144b676a15da5
Jace Babin <jbabin91@gmail.com>
2020-07-07 17:19:04 -0700

    Started working on api
    Signed-off-by: Jace Babin <jbabin91@gmail.com>

---
+++ backend/src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+const compression = require('compression');
+const helmet = require('helmet');
+
+const middleware = require('./middleware');
+const project = require('./constants/project');
+
+const app = express();
+
+app.use(morgan('tiny'));
+app.use(compression());
+app.use(helmet());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: project.message,
+  });
+});
+
+app.use(middleware.notFound);
+app.use(middleware.errorHandler);
+
+module.exports = app;
---
+++ backend/src/constants/project.js
@@ -0,0 +1,5 @@
+const message = 'Cookbook API';
+
+module.exports = {
+  message,
+};
